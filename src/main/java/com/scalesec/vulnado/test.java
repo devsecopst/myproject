423a7a6f3e8a1c04292686ed05a7591cd99b4f75
devsecopst <devsecopst@gmail.com>
2025-05-21 19:09:07 +0300

    testing vulnerable

---
+++ src/main/java/com/scalesec/vulnado/test.java
@@ -0,0 +1,8 @@
+package com.scalesec.vulnado;
+
+public class test {
+    public static void main(String[] args) {
+        String password = "superSecret123"; // hardcoded password
+        System.out.println("This is bad practice!");
+    }
+}
