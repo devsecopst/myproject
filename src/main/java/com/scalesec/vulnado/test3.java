ab9a2513367e301009e545afc3a45be3e9d38e4d
devsecopst <devsecopst@gmail.com>
2025-05-21 22:33:31 +0300

    testing vulnerable

--- src/main/java/com/scalesec/vulnado/test3.java
+++
@@ -1,7 +0,0 @@
-public class TestSemgrep {
-
-    public static void main(String[] args) {
-        String password = "superSecret123"; // hardcoded password
-        System.out.println("This is bad practice!");
-    }
-}
