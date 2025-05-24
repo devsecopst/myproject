d0a492d5f674a7df2ec8da25c193cb281c711496
devsecopst <devsecopst@gmail.com>
2025-05-24 15:47:03 +0300

    new update by emran 7

--- semgrep-warn-only.sh
+++ semgrep-warn-only.sh
@@ -4,14 +4,12 @@ echo "🔍 Running Semgrep scan (warn-only)..."
 semgrep \
   --config p/gitleaks \
   --config p/secrets \
-  --config p/comment \
   --config p/cwe-top-25 \
-  --config p/default \
   --config p/owasp-top-ten \
   --config p/java \
   --config p/dockerfile \
   --config p/docker-compose \
-  --config p/security-code-scan \
+  --config p/findsecbugs \
   --skip-unknown-extensions \
   --disable-version-check \
   --metrics=off \
