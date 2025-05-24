7f6ebb882c6e23e62f6f5a4f6068a745b0970fa1
devsecopst <devsecopst@gmail.com>
2025-05-24 12:40:18 +0300

    new changes16

--- src/main/java/com/scalesec/vulnado/SQLinjection.java
+++ src/main/java/com/scalesec/vulnado/SQLinjection.java
@@ -16,6 +16,7 @@ public class SQLInjection {
             String query = "SELECT * FROM books WHERE username = '" + userInput + "'";
             String query = "SELECT * FROM books WHERE username = '" + userInput + "'";
             String query = "SELECT * FROM books WHERE username = '" + userInput + "'";
+            String query = "SELECT * FROM people WHERE username = '" + userInput + "'";
             while (rs.next()) {
                 System.out.println("User found: " + rs.getString("username"));
             }
